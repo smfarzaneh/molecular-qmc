c108f5cdc6781ea85b19c9b1026f5875344116b9
smfarzaneh <smf552@nyu.edu>
2017-12-11 20:23:16 -0500

    added nucleus-nucleus potential

--- src/nbody.py
+++ src/nbody.py
@@ -1,3 +1,4 @@
+from trialWavefunction import *
 import numpy as np
 
 class Nbody(object):
@@ -7,16 +8,24 @@ class Nbody(object):
 		self.K = numNuclei
 		self.N = numElectrons
 		self.Z = nucleiCharges
+		self.setNucleiPositions(nucleiPositions)
+		self.setElectronPositions(electronsPositions)
+
+	def setNucleiPositions(self, nucleiPositions):
+		
 		self.Rx = nucleiPositions[0, :]
 		self.Ry = nucleiPositions[1, :]
 		self.Rz = nucleiPositions[2, :]
+
+	def setElectronPositions(self, electronsPositions):
+		
 		self.rx = electronsPositions[0, :]
 		self.ry = electronsPositions[1, :]
 		self.rz = electronsPositions[2, :]
 
-	def distanceToNuclei(self):
+	def distanceElectronNucleus(self):
 
-		# calculate all electrons-nuclei distances 
+		# calculate all electron-nucleus distances 
 		# return them as a matrix with N rows and K columns
 
 		# tile positions into matrices to avoid nested loops
@@ -35,9 +44,9 @@ class Nbody(object):
 
 		return distance
 
-	def distanceToElectrons(self):
+	def distanceElectronElectron(self):
 
-		# calculate all electrons-electrons distances 
+		# calculate all electron-electron distances 
 		# return them as a matrix with N rows and N columns
 
 		# tile positions into matrices to avoid nested loops
@@ -56,18 +65,39 @@ class Nbody(object):
 
 		return distance
 
-	def nucleiPotential(self):
+	def distanceNucleusNucleus(self):
 
-		D = self.distanceToNuclei()
+		# calculate all nucleus-nucleus distances 
+		# return them as a matrix with K rows and K columns
+
+		# tile positions into matrices to avoid nested loops
+		Rx_col = np.tile(self.Rx, (self.K, 1))
+		Ry_col = np.tile(self.Ry, (self.K, 1))
+		Rz_col = np.tile(self.Rz, (self.K, 1))
+		Rx_row = np.tile(self.Rx, (self.K, 1)).transpose()
+		Ry_row = np.tile(self.Ry, (self.K, 1)).transpose()
+		Rz_row = np.tile(self.Rz, (self.K, 1)).transpose()
+
+		# calculate distances
+		dx = np.subtract(Rx_row, Rx_col)
+		dy = np.subtract(Ry_row, Ry_col)
+		dz = np.subtract(Rz_row, Rz_col)
+		distance = np.sqrt(np.power(dx, 2) + np.power(dy, 2) + np.power(dz, 2))
+
+		return distance
+
+	def potentialElectronNucleus(self):
+
+		D = self.distanceElectronNucleus()
 		RD = np.reciprocal(D)
 		Z = np.tile(self.Z, (self.N, 1))
 		potential = -np.sum(np.multiply(Z, RD))
 
 		return potential
 
-	def electronsPotential(self):
+	def potentialElectronElectron(self):
 
-		D = self.distanceToElectrons()
+		D = self.distanceElectronElectron()
 		I = np.identity(self.N)
 		RD = np.reciprocal(D + I) - I
 		potential = np.sum(RD)/2.0 		
@@ -75,8 +105,49 @@ class Nbody(object):
 
 		return potential
 
+	def potentialNucleusNucleus(self):
+
+		D = self.distanceNucleusNucleus()
+		I = np.identity(self.K)
+		RD = np.reciprocal(D + I) - I
+		potential = np.sum(RD)/2.0 		
+		# divide by 2 is for counting nucleus-nucleus distances twice
+
+		return potential
+
 	def potential(self):
 
-		return self.nucleiPotential() + self.electronsPotential()
+		return self.potentialElectronNucleus() \
+			+ self.potentialElectronElectron() \
+			+ self.potentialNucleusNucleus()
 
-	
+	def kinetic(self):
+
+		h = 0.001
+		r = np.array([[self.rx], [self.ry], [self.rz]])
+		r_forward = r + h
+		r_backward = r - h
+		wavefunction_r = TrialWavefunction.evaluate(r)
+		wavefunction_r_forward = TrialWavefunction.evaluate(self.r_forward)
+		wavefunction_r_backward = TrialWavefunction.evaluate(self.r_backward)
+		secondDerivative = (wavefunction_r_forward + wavefunction_r_backward - 2.0*wavefunction_r)/h**2
+		laplacian = np.sum(secondDerivative)
+		kinetic = -0.5*laplacian/wavefunction_r
+
+		return kinetic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
