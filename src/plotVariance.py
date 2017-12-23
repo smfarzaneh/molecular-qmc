f93ead7ac3f6cf4084d3cd2fd9541a8603cda5c8
smfarzaneh <smf552@nyu.edu>
2017-12-23 12:57:29 -0500

    calculate variance cumulatively

--- src/plotVariance.py
+++ src/plotVariance.py
@@ -3,34 +3,44 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 from monteCarlo import *
-from hydrogenMolecule import *
+from hydrogen import *
 
-hm = HydrogenMolecule(1.4)
-mc = MonteCarlo(hm, 2.0)
+# initialization
+hm = Hydrogen()
+mc = MonteCarlo(hm, 2.5)
 
-alpha = 1.1
-powers = np.arange(8, 17)
+alpha = 1.0
+powers = np.arange(8, 18)
 numSample = np.power(2, powers)
 energy = np.zeros(len(numSample))
+energy2 = np.zeros(len(numSample))
 var = np.zeros(len(numSample))
-
+N = 0
+lastEnergy = 0
+lastEnergy2 = 0
 
 # prepare figure
 fig, ax = plt.subplots(1,1)
 
 for i in range(len(numSample)):
 	
-	N = numSample[i]
-	en = np.zeros(N)
+	N = numSample[i] - N
 	for iteration in range(N):
 		mc.sample(alpha)
-		en[iteration] = mc.molecule.localEnergy(alpha)
-	print 'N = ' + str(N) + ', accepted: ' + str(mc.numAccepted/(N + 0.0)*100) + '%'
+		localEnergy = mc.molecule.localEnergy(alpha)
+		energy[i] += localEnergy
+		energy2[i] += localEnergy**2
 	# calculate mean energy and variance 
-	energy[i] = np.sum(en)/float(N)
-	var[i] = np.sum((en - energy[i])**2)/float(N*(N - 1))
+	energy[i] += lastEnergy
+	energy2[i] += lastEnergy2
+	energy[i] /= float(numSample[i])
+	var[i] = energy2[i]/float(numSample[i]) - energy[i]**2
+	print 'N = ' + str(numSample[i]) + ', energy=' + str(energy[i]) + ', variance=' + str(var[i]) + ', accepted: ' + str(mc.numAccepted*100/N) + '%'
+	lastEnergy = energy[i]*numSample[i]
+	lastEnergy2 = energy2[i]
+	
 
-ax.plot(numSample, var)
+ax.plot(numSample, var, 'k-*')
 
 ax.set_xlabel(r'N')
 ax.set_ylabel(r'variance $\sigma^2$')
